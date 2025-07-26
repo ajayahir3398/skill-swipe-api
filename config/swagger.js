13e6fcd4e9a4a78f9e7ad4610ebcde6fe550ffb4
AjayB <bandhiya.ajay@gmail.com>
2025-07-26 16:28:14 +0530

    swagger config

--- config/swagger.js
+++ config/swagger.js
@@ -14,7 +14,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://skill-swipe-api-o7ci.onrender.com',
         description: 'Development server'
       }
     ],
